a0e43e223cf9bc1f60863fd99f7ad22dac0e9aae
yocxli <yocxli@gmail.com>
2014-01-18 16:13:37 +0900

    テーブルレコード一覧画面のリストビューをオートローディングに修正

--- MediaStoreChecker/src/net/yocxli/mediastorechecker/app/MediaStoreFragment.java
+++ MediaStoreChecker/src/net/yocxli/mediastorechecker/app/MediaStoreFragment.java
@@ -13,25 +13,38 @@ import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MediaStoreFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class MediaStoreFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>,
+        OnScrollListener {
     
     private static final String TAG = "MediaStoreFragment";
-    private static final boolean LOCAL_LOGV = false;
+    private static final boolean LOCAL_LOGV = true;
     
-    public static final String TARGET_URI = "target.uri";
+    public static final String FRAGMENT_ARGUMENT_TARGET_URI = "target.uri";
+    
+    public static final String LOADER_ARGUMENT_PAGE = "page";
+    
+    public static final int AUTOLOAD_THRESHHOLD = 5;
+    public static final int PAGE_SIZE           = 100;
     
     private SimpleCursorAdapter mAdapter;
     private Uri mUri;
+    
+    private boolean mIsLoading;
+    private boolean mIsLoadedAll = false;
+    private int mPage = 0;
+    private int mLastItemCount;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         
         Bundle args = getArguments();
-        mUri = (Uri) args.getParcelable(TARGET_URI);
+        mUri = (Uri) args.getParcelable(FRAGMENT_ARGUMENT_TARGET_URI);
         
         setEmptyText(getResources().getString(R.string.message_no_data));
         
@@ -41,8 +54,11 @@ public class MediaStoreFragment extends ListFragment implements LoaderManager.Lo
                 null, getProjection(mUri), new int[] {android.R.id.text1, android.R.id.text2}, 0);
         
         setListAdapter(mAdapter);
+        getListView().setOnScrollListener(this);
         
-        getLoaderManager().initLoader(0, null, this);
+        Bundle loaderArgs = new Bundle();
+        loaderArgs.putInt(LOADER_ARGUMENT_PAGE, mPage);
+        getLoaderManager().initLoader(0, loaderArgs, this);
     }
 
     @Override
@@ -60,8 +76,13 @@ public class MediaStoreFragment extends ListFragment implements LoaderManager.Lo
         if (LOCAL_LOGV) {
             Log.v(TAG, "onCreateLoader: id=" + id);
         }
+        mIsLoading = true;
+        
+        int page = args.getInt(LOADER_ARGUMENT_PAGE, 0);
+        String limit = String.valueOf(((page * PAGE_SIZE) + PAGE_SIZE));
+        
         Uri.Builder builder = mUri.buildUpon();
-        builder.appendQueryParameter("limit", "100");
+        builder.appendQueryParameter("limit", limit);
         return new CursorLoader(getActivity(),
                 builder.build(),
                 getProjection(mUri),
@@ -73,7 +94,14 @@ public class MediaStoreFragment extends ListFragment implements LoaderManager.Lo
         if (LOCAL_LOGV) {
             Log.v(TAG, "onLoadFinished: data=" + data);
         }
+        mIsLoading = false;
         mAdapter.swapCursor(data);
+        final int count = mAdapter.getCount();
+        if (mLastItemCount == count) {
+            // 前回読み込み時とアイテム数が一緒なら、全データ読み込み済みのフラグを立てる。
+            mIsLoadedAll = true;
+        }
+        mLastItemCount = count;
         if (isResumed()) {
             setListShown(true);
         } else {
@@ -102,5 +130,23 @@ public class MediaStoreFragment extends ListFragment implements LoaderManager.Lo
             projection[1] = MediaStore.MediaColumns.DATA;
         }
         return projection;
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem,
+            int visibleItemCount, int totalItemCount) {
+        if (totalItemCount == 0) {
+            return;
+        }
+        if (!mIsLoadedAll && !mIsLoading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + AUTOLOAD_THRESHHOLD)) {
+            mIsLoading = true;
+            Bundle loaderArgs = new Bundle();
+            loaderArgs.putInt(LOADER_ARGUMENT_PAGE, ++mPage);
+            getLoaderManager().restartLoader(0, loaderArgs, this);
+        }
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
     }
 }
--- MediaStoreChecker/src/net/yocxli/mediastorechecker/app/MediaStoreTableActivity.java
+++ MediaStoreChecker/src/net/yocxli/mediastorechecker/app/MediaStoreTableActivity.java
@@ -47,62 +47,62 @@ public class MediaStoreTableActivity extends ActionBarActivity {
             typeText = getString(R.string.label_image);
             
             Bundle imageExternalArgs = new Bundle();
-            imageExternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            imageExternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, imageExternalArgs);
             
             Bundle imageInternalArgs = new Bundle();
-            imageInternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            imageInternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_internal), MediaStoreFragment.class, imageInternalArgs);
         } else if (mType == Const.MEDIA_TYPE_IMAGE_THUMBNAIL) {
             // 画像サムネイル
             typeText = getString(R.string.label_image_thumbnail);
             
             Bundle imageExternalThumbnailArgs = new Bundle();
-            imageExternalThumbnailArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI);
+            imageExternalThumbnailArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, imageExternalThumbnailArgs);
             
             Bundle imageInternalThumbnailArgs = new Bundle();
-            imageInternalThumbnailArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Images.Thumbnails.INTERNAL_CONTENT_URI);
+            imageInternalThumbnailArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Images.Thumbnails.INTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_internal), MediaStoreFragment.class, imageInternalThumbnailArgs);
         } else if (mType == Const.MEDIA_TYPE_AUDIO) {
             // 音楽
             typeText = getString(R.string.label_audio);
             
             Bundle audioExternalArgs = new Bundle();
-            audioExternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+            audioExternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, audioExternalArgs);
             
             Bundle audioInternalArgs = new Bundle();
-            audioInternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Audio.Media.INTERNAL_CONTENT_URI);
+            audioInternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Audio.Media.INTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_internal), MediaStoreFragment.class, audioInternalArgs);
         } else if (mType == Const.MEDIA_TYPE_AUDIO_ALBUM) {
             // アルバム
             typeText = getString(R.string.label_audio_album);
             
             Bundle audioAlbumExternalArgs = new Bundle();
-            audioAlbumExternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI);
+            audioAlbumExternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, audioAlbumExternalArgs);
             
             Bundle audioAlbumInternalArgs = new Bundle();
-            audioAlbumInternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Audio.Albums.INTERNAL_CONTENT_URI);
+            audioAlbumInternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Audio.Albums.INTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_internal), MediaStoreFragment.class, audioAlbumInternalArgs);
         } else if (mType == Const.MEDIA_TYPE_AUDIO_ARTIST) {
             // アーティスト
             typeText = getString(R.string.label_audio_artist);
             
             Bundle audioArtistExternalArgs = new Bundle();
-            audioArtistExternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI);
+            audioArtistExternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, audioArtistExternalArgs);
             
             Bundle audioArtistInternalArgs = new Bundle();
-            audioArtistInternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Audio.Artists.INTERNAL_CONTENT_URI);
+            audioArtistInternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Audio.Artists.INTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_internal), MediaStoreFragment.class, audioArtistInternalArgs);
         } else if (mType == Const.MEDIA_TYPE_AUDIO_GENRE) {
             // ジャンル
             typeText = getString(R.string.label_audio_genre);
             
             Bundle audioGenreExternalArgs = new Bundle();
-            audioGenreExternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI);
+            audioGenreExternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Audio.Genres.EXTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, audioGenreExternalArgs);
             
             /* no such table で落ちる（Xperia AX）
@@ -115,7 +115,7 @@ public class MediaStoreTableActivity extends ActionBarActivity {
             typeText = getString(R.string.label_audio_playlist);
             
             Bundle audioPlaylistExternalArgs = new Bundle();
-            audioPlaylistExternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI);
+            audioPlaylistExternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, audioPlaylistExternalArgs);
             
             /* no such table で落ちる (Xperia AX)
@@ -128,22 +128,22 @@ public class MediaStoreTableActivity extends ActionBarActivity {
             typeText = getString(R.string.label_video);
             
             Bundle videoExternalArgs = new Bundle();
-            videoExternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+            videoExternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, videoExternalArgs);
             
             Bundle videoInternalArgs = new Bundle();
-            videoInternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Video.Media.INTERNAL_CONTENT_URI);
+            videoInternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Video.Media.INTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_internal), MediaStoreFragment.class, videoInternalArgs);
         } else if (mType == Const.MEDIA_TYPE_VIDEO_THUMBNAIL) {
             // 動画サムネイル
             typeText = getString(R.string.label_video_thumbnail);
             
             Bundle videoThumbnailExternalArgs = new Bundle();
-            videoThumbnailExternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Video.Thumbnails.EXTERNAL_CONTENT_URI);
+            videoThumbnailExternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Video.Thumbnails.EXTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, videoThumbnailExternalArgs);
             
             Bundle videoThumbnailInternalArgs = new Bundle();
-            videoThumbnailInternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Video.Thumbnails.INTERNAL_CONTENT_URI);
+            videoThumbnailInternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Video.Thumbnails.INTERNAL_CONTENT_URI);
             mTabsAdapter.addTab(getString(R.string.volume_internal), MediaStoreFragment.class, videoThumbnailInternalArgs);
         } else if (mType == Const.MEDIA_TYPE_FILE) {
             // ファイル
@@ -151,11 +151,11 @@ public class MediaStoreTableActivity extends ActionBarActivity {
             
             if (MediaStoreHelper.hasFileTable()) {
                 Bundle fileExternalArgs = new Bundle();
-                fileExternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Files.getContentUri("external"));
+                fileExternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Files.getContentUri("external"));
                 mTabsAdapter.addTab(getString(R.string.volume_external), MediaStoreFragment.class, fileExternalArgs);
                 
                 Bundle fileInternalArgs = new Bundle();
-                fileInternalArgs.putParcelable(MediaStoreFragment.TARGET_URI, MediaStore.Files.getContentUri("internal"));
+                fileInternalArgs.putParcelable(MediaStoreFragment.FRAGMENT_ARGUMENT_TARGET_URI, MediaStore.Files.getContentUri("internal"));
                 mTabsAdapter.addTab(getString(R.string.volume_internal), MediaStoreFragment.class, fileInternalArgs);
             }
         }
